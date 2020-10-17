fe8d57a29180d9790eb722ba4c4183784357796e
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 18:29:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,19 +5,20 @@ def key_for_min_value(name_hash)
   first_item = true 
   min_val = 100
   min_val_key = :key 
-  name_hash.each do |key, value|
-    if name_hash == {}
-      return nil 
-    elsif first_item
-      first_item = false 
-      min_val = value
-      min_val_key = key 
-    else
-      if value < min_val
+  if name_hash == {}
+    return nil 
+  else 
+    name_hash.each do |key, value|
+      if first_item
+        first_item = false 
         min_val = value
         min_val_key = key 
+      else
+        if value < min_val
+          min_val = value
+          min_val_key = key 
+        end
       end
     end
-  end
   min_val_key
 end
